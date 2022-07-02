2c698037ff6838c84bb4a10dc8c6189f3ec8334b
Monokuro <07akhmadnur@gmail.com>
2022-07-02 18:22:54 +0700

    Update db score & API for job, applicant score

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+// Import controller
 const jobs = require("../controllers/jobs");
 
 // Get jobs for dropdown menu
 router.get("/getJobs", jobs.getJobs);
 
+// Get job description
+router.post("/getJobDesc", jobs.getJobDesc);
+
+// Get job responsibility
+router.post("/getJobRes", jobs.getJobRes);
+
+// Get job qualification
+router.post("/getJobQua", jobs.getJobQua);
+
+// Get applicant score
+router.post("/getApplicantScoreByJob", jobs.getApplicantScoreByJob);
+
+// Export module
 module.exports = router;
