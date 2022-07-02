d92e1644260028efdec8031f90c2104ec952b56d
Monokuro <07akhmadnur@gmail.com>
2022-07-02 16:02:53 +0700

    Get jobs & update gitignore

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,9 +1,19 @@
 const { PrismaClient } = require("@prisma/client");
 const db = new PrismaClient();
 
+// Get jobs for dropdown menu
 const getJobs = async (req, res) => {
-  const applicant = await db.applicant.findMany();
-  res.status(200).json({ applicant: applicant });
+  try {
+    const jobs = await db.job.findMany({
+      select: {
+        id_job: true,
+        job_name: true,
+      },
+    });
+    res.status(200).json({ jobs });
+  } catch (err) {
+    throw "Something Wrong";
+  }
 };
 
 module.exports = {
--- routes/jobs.js
+++ routes/jobs.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const jobs = require("../controllers/jobs");
 
+// Get jobs for dropdown menu
 router.get("/getJobs", jobs.getJobs);
 
 module.exports = router;
