81bb8b7eefaf591dd493b7de99bf7efa6571b966
Monokuro <07akhmadnur@gmail.com>
2022-07-03 17:06:31 +0700

    set applicant route to post

--- routes/applicant.js
+++ routes/applicant.js
@@ -4,18 +4,18 @@ const router = express.Router();
 const applicant = require("../controllers/applicant");
 
 // mengambil detail profil score
-router.get("/score", applicant.score);
+router.post("/score", applicant.score);
 // mengambil detail profil pengalaman kerja
-router.get("/experience", applicant.pengalamanKerja);
+router.post("/experience", applicant.pengalamanKerja);
 // memanggil detail profil sentimen
-router.get("/sentimen", applicant.sentimen);
+router.post("/sentimen", applicant.sentimen);
 // memanggil detail profil keahlian
-router.get("/keahlian", applicant.keahlian);
+router.post("/keahlian", applicant.keahlian);
 // memanggil detail profil tolerant
-router.get("/intolerant", applicant.intolerant);
+router.post("/intolerant", applicant.intolerant);
 // memanggil detail profil Ketertarikan
-router.get("/ketertarikan", applicant.ketertarikan);
+router.post("/ketertarikan", applicant.ketertarikan);
 // memanggil detail profil kepribadian
-router.get("/kepribadian", applicant.kepribadian);
+router.post("/kepribadian", applicant.kepribadian);
 
 module.exports = router;
